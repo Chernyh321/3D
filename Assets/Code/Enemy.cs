b8a565f382b0229c16fe099d66daf159964b9127
Chernyh321 <katk16022@gmail.com>
2026-02-07 20:18:57 +0800

    разбирались со скриптом врагов

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed;
+    public float speed=1.0f;
     public Transform target;
     public int playerDamage=2;
+void Update(){
+    transform.position=Vector3.MoveTowards(transform.position,target.position,speed*Time.deltaTime);
+    transform.LookAt(target.position);
 
 
+}
+private void OnTriggerEnter(Collider other){
+    Player player=other.GetComponent<Player>();
+        player. TakeDamage(playerDamage);
+}
 }
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -2,26 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour{
     private int health = 10;
-    private int coins;
-    public AudioSource audioSource;
-    public AudioClip damageSound; 
-    public AudioClip collectSound; 
+
 
     public void TakeDamage(int damage){
-        health-=damage  //отнимает здоровье
+        health-=damage;  //отнимает здоровье
         print("Здоровье игрока:" + health); //Сообщает о здоровье игрока
-
-
     }
-
-    public void CollecCoint()
-    {
-        coins++;    //Увеличивает кол-во менет на 1
-        audioSource.PlayOneShot(collectSound);  //Проишрывает звук
-        print("Собранные монетки:"+coins);  //Сообщает о собранных монетках
-    }
-     
 }
