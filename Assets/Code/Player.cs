af49f0c47fce683dae77ffb2ecb81c7222c1ced7
Chernyh321 <katk16022@gmail.com>
2026-03-07 20:15:02 +0800

    исправлялили ошибки, ну и писали скрипты

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class Player : MonoBehaviour{
     private int health = 10;
     public int coins;
+    public AudioSource audioSource;
+    public AudioClip damageSound;
 
     public GameObject fireballPrefab;
     public Transform attackPoint;
@@ -12,6 +16,16 @@ public class Player : MonoBehaviour{
     public void TakeDamage(int damage){
         health-=damage;  //отнимает здоровье
         print("Здоровье игрока:" + health); //Сообщает о здоровье игрока
+
+        if(health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 
 void Update(){
@@ -26,3 +40,5 @@ public void CollectCoins(){
     coins+=1;
 }
 }
+
+
