9a2c39ed3b8ce6abf8fdd0acb93d0f845fd2f412
Floxikwt <katk16022@gmail.com>
2026-04-25 20:21:48 +0800

    Разработка второго уровня игры

--- Assets/Code/TemperatureUI.cs
+++ Assets/Code/TemperatureUI.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class TemperatureUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public  Health health;
+    public int playerDamage=2;
+    public float tempetatureCurrent = 36.6f;
+    public float tempetatureNormal = 36.6f;
+    public float temperatureCritical = 34f;
+    public float freezeSpeed = 0.05f;
+    public float freezeDamageTimer = 1;
+    public float freezeDamageDelay = 2;
+    public float temperature;
 
-    // Update is called once per frame
     void Update()
     {
         
+        if (tempetatureCurrent <= temperatureCritical) 
+        {
+            if (freezeDamageTimer <= 0)
+            {
+                health.TakeDamage(playerDamage);
+                freezeDamageTimer += freezeDamageDelay;
+            }
+            else
+            {
+                freezeDamageTimer -= Time.deltaTime;
+
+            }
+
+        }
+        Mathf.Round(temperature * 10.0f)* 0.1f
+
+
     }
 }
