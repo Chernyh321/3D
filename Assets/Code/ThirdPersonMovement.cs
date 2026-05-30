fa2bb7cae33bfa5907441ce2d68a188cd276dfd6
milk20090 <alhahay0603@gmail.com>
2026-05-30 20:20:19 +0800

    vgvg

---
+++ Assets/Code/ThirdPersonMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovemend : MonoBehaviour
+{
+    public float speed = 5f;
+    public float runSpeed = 10f;
+    float targetMovingSpeed;
+    public bool canRun = true;
+    public bool isRunning;
+    public KeyCode runnihgKey = KeyCode.LeftShift;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
