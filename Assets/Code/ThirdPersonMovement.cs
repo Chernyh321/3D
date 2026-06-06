ed4f7bdd247d535782455c39fdef157628400d01
Stepak1lkaVilka <mishaalex35@mail.ru>
2026-06-06 20:41:34 +0800

    ggg

--- Assets/Code/ThirdPersonMovement.cs
+++ Assets/Code/ThirdPersonMovement.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonMovemend : MonoBehaviour
+public class ThirdPersonMovement : MonoBehaviour
 {
+    private Rigidbody playerRigidbody;
     public float speed = 5f;
     public float runSpeed = 10f;
     float targetMovingSpeed;
@@ -20,4 +21,36 @@ public class ThirdPersonMovemend : MonoBehaviour
     {
         
     }
+
+    void Awake(){
+        playerRigidbody = this.GetComponent<Rigidbody>();
+
+    }
+
+
+    void FixedUpdate(){
+        if(canRun &&  Input.GetKey(runnihgKey))
+        {
+            isRunning=true;
+        }
+        else
+        {
+            isRunning=false;
+        }
+
+        if(isRunning)
+        {
+            targetMovingSpeed = runSpeed;
+        }
+        else
+        {
+            targetMovingSpeed = speed;
+        }
+
+        playerRigidbody.velocity = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), playerRigidbody.velocity.y, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * (100f * Time.deltaTime), Space.Self);
+    }
+
+
 }
